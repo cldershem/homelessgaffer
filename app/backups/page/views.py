4c677e37c2c7d00e8d577b0c102be37c3c483e27
Cameron Dershem <cldershem@gmail.com>
2013-11-20 04:21:05 -0500

    add app/backups jic

---
+++ app/backups/page/views.py
@@ -0,0 +1,94 @@
+from flask import (Blueprint, render_template, flash, request,
+                   redirect, url_for)
+from jinja2 import TemplateNotFound
+from app.models import Page, User
+from app.forms import PageForm
+from app.constants import DATE_TIME_NOW
+from app.utils import makeSlug, markRight
+from flask.ext.login import login_required, current_user
+
+mod = Blueprint('static_page', __name__, url_prefix='/page')
+
+
+@mod.route('/')
+@mod.route('/listpages')
+def listPages():
+    pages = Page.objects.all()
+    return render_template('page/listPages.html',
+                           page="listpages",
+                           pages=pages)
+
+
+@mod.route('/<slug>')
+def staticPage(slug):
+    try:
+        return render_template('page/%s.html' % slug,
+                               page=slug)
+    except TemplateNotFound:
+        page = Page.objects.get_or_404(slug=slug)
+        content = markRight(page.content)
+        return render_template('page/staticpage.html',
+                               title=page.title,
+                               slug=page.slug,
+                               content=content)
+
+
+@mod.route('/newpage', methods=['GET', 'POST'])
+@login_required
+def newPage():
+    form = PageForm()
+
+    if request.method == 'POST':
+        slug = makeSlug(form.title.data)
+        if form.validate() is False:
+            return render_template("page/newPage.html", form=form)
+        else:
+            # if form.isDraft.data:
+                # save as form.title.data + "-draft"
+            # elif !form.isDraft.data:
+                # save as draft?
+                # send to preview confirm page
+                    # if confirm
+                        # remove "draft" indicator
+                        # publish
+                    # else:
+                        # redirect to edit page
+                        # flash "edit or save as draft"
+            # if isBlogPost:
+                # do this
+            newPage = Page(title=form.title.data,
+                           slug=slug, content=form.content.data)
+            newPage.author = User.objects.get(email=current_user.email)
+            newPage.save()
+            flash('Your page has been posted')
+            return redirect(url_for('.staticPage', slug=slug))
+    elif request.method == 'GET':
+        return render_template("page/newPage.html", form=form, page="newpage")
+
+
+@mod.route('/<slug>/edit', methods=['GET', 'POST'])
+@login_required
+def editPage(slug):
+    page = Page.objects.get(slug=slug)
+    slug = page.slug
+    form = PageForm(obj=page)
+
+    if request.method == 'POST':
+        if form.validate() is False:
+            return render_template('page/editPage.html', title=page.title,
+                                   slug=slug, form=form)
+        else:
+            form.populate_obj(page)
+            page.edited_on.append(DATE_TIME_NOW)
+            page.save()
+            flash("Your page has been updated.")
+            return redirect(url_for('.staticPage', slug=slug))
+    elif request.method == 'GET':
+        form.populate_obj(page)
+        return render_template('page/editPage.html', title=page.title,
+                               slug=slug, form=form)
+
+
+@mod.route('/food')  # is food part of posts/blog now?
+def food():
+    return redirect(url_for('blog.listPosts', tag="food"))
